9c4146bde8f7b81bd5e7f0588961d6f5e49d0148
Evan McGinnis <evan@katanagraph.com>
2021-12-09 15:16:08 -0600

    script importing smiles_small

---
+++ rdg_datasets/smiles_small/import.py
@@ -0,0 +1,26 @@
+import os
+import pathlib
+
+from libuprev import rdg_import
+from libuprev.uprev_config import Config
+import csv_datasets
+
+# vars unique to this rdg
+local_path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+csv_dataset = "smiles_small"
+csv_path = csv_datasets.csv_dataset_dir / csv_dataset
+import_args = ["--ids-are-integers", "--files-have-headers"]
+node_file = "node_schema.txt"
+edge_file = "edge_schema.txt"
+
+def uprev(config: Config, new_storage_format_version: int) -> pathlib.Path:
+    available_csv = csv_datasets.available_csv()
+    if csv_dataset not in available_csv:
+        raise RuntimeError("csv dataset {} not in available csv_datasets: {}".format(csv_dataset, available_csv))
+    return rdg_import.import_(config=config,
+                              rdg_path=local_path,
+                              out_ver=new_storage_format_version,
+                              csv_path=csv_path,
+                              node_file=node_file,
+                              edge_file=edge_file,
+                              import_args=import_args)
