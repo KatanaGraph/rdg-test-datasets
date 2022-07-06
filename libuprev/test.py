1789dbead9f54bac99002be9d4fee335ace56bb6
Evan McGinnis <evan@katanagraph.com>
2022-07-06 11:32:38 -0500

    begin adding unit tests

---
+++ libuprev/test.py
@@ -0,0 +1,11 @@
+
+import rdg_datasets
+from libuprev import color, constants, fs, tools
+
+# TODO
+# - check rdg dir organization
+# - enforce presence of an uprev script
+# - enforce that the rdg is stored in the latest sfv
+
+
+def test_organization(ctx, test_datasets_dir: str):
