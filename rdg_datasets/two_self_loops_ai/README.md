a536eb7b38914c1f881e95169299635d62a3e211
Loc Hoang <loc@katanagraph.com>
2022-07-13 17:47:21 +0000

    Change two_self_loops_ai input to use a generate script
    Since I added the infrastructure to use generate scripts for AI inputs,
    it only makes sense to change the way two_self_loops_ai is generated as
    well.
    
    This commit removes the migrate/add_features scripts and replaces them
    with a single generate script. Version 8 has been regenerated with this
    script to make sure it works as well.

--- rdg_datasets/two_self_loops_ai/README.md
+++ rdg_datasets/two_self_loops_ai/README.md
@@ -3,18 +3,7 @@ suffix because it has filler labels, splits, and features.
 The features in question are 2 length vectors with 1 1 for the first node
 and 2 2 for the second node
 
-################################################################################
 # Generation
-################################################################################
 
-Files used to generate this graph are in the generation directory.
-The first part of this is generated from a csv, but the later part of it
-requires running a script on the generated RDG. This is required because csv-import
-doesn't support adding feature vectors.
-
-First run csv-import from csv_datasets/two_self_loops_ai to get the topology:
-./csv-import node_schema.txt  edge_schema.txt  two_self_loops_ai --ids-are-integers
-
-After that, with the Katana distributed environment in Python path and the output RDG in
-the same directory, run the add features script:
-python3 add_features.py
+Use uprev to generate the graph (requires access to a katana_enterprise build). See the
+top-level README for uprev instructions.
---
+++ rdg_datasets/two_self_loops_ai/generate.py
@@ -0,0 +1,45 @@
+import os
+import pathlib
+import subprocess
+
+import csv_datasets
+from libuprev import rdg_import
+from libuprev.uprev_config import Config
+
+local_path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+csv_dataset = "two_self_loops_ai"
+csv_path = csv_datasets.csv_dataset_dir / csv_dataset
+import_args = ["--ids-are-integers"]
+node_file = "node_schema.txt"
+edge_file = "edge_schema.txt"
+
+
+def uprev(config: Config, new_storage_format_version: int) -> pathlib.Path:
+    available_csv = csv_datasets.available_csv()
+    if csv_dataset not in available_csv:
+        raise RuntimeError("csv dataset {} not in available csv_datasets: {}".format(csv_dataset, available_csv))
+
+    # imports topology first into an RDG
+    return_path = rdg_import.import_(
+        config=config,
+        rdg_path=local_path,
+        out_ver=new_storage_format_version,
+        csv_path=csv_path,
+        node_file=node_file,
+        edge_file=edge_file,
+        import_args=import_args,
+    )
+
+    # path to the built Katana python environment
+    python_env_path = config.build_dir / "python_env.sh"
+    # path to feature adding script in an enterprise build
+    script_path = (
+        config.build_dir / "katana_enterprise_python_build/python/test/datagen/two_self_loops_ai_features.py"
+    )
+
+    # run script to add features to the RDG (in-place)
+    cmd = [python_env_path, "python3", script_path, return_path.absolute()]
+    subprocess.run(cmd, check=True)
+
+    # returns the original path as required by uprev
+    return return_path
